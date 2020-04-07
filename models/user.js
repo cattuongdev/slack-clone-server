4b5ed3302d689b8026e5b66eefb47a9842dc607f
Tuong Huynh <tuong.huynh@hdwebsoft.co>
2020-04-07 15:16:03 +0700

    login mutation

--- models/user.js
+++ models/user.js
@@ -1,19 +1,31 @@
 export default (sequelize, DataTypes) => {
-  const User = sequelize.define(
-    'user',
-    {
-      username: {
-        type: DataTypes.STRING,
-        unique: true,
+  const User = sequelize.define('user', {
+    username: {
+      type: DataTypes.STRING,
+      unique: true,
+      validate: {
+        isAlphanumeric: {
+          args: true,
+          msg: 'The username can only contain letters and numbers',
+        },
+        len: {
+          args: [3, 25],
+          msg: 'The username needs to be between 3 and 25 characters long',
+        },
       },
-      email: {
-        type: DataTypes.STRING,
-        unique: true,
-      },
-      password: DataTypes.STRING,
     },
-    { underscored: true },
-  );
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      validate: {
+        isEmail: {
+          args: true,
+          msg: 'Invalid email',
+        },
+      },
+    },
+    password: DataTypes.STRING,
+  });
 
   User.associate = (models) => {
     User.belongsToMany(models.Team, {
@@ -24,7 +36,7 @@ export default (sequelize, DataTypes) => {
       },
     });
     // N:M
-    User.belongsToMany(models.Team, {
+    User.belongsToMany(models.Channel, {
       through: 'channel_member',
       foreignKey: {
         name: 'userId',
